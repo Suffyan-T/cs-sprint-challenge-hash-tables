b69e620a8052cae7c07ce02fe64e3892341ded3e
Suffyan Tariq <suffyan.tariq@gmail.com>
2020-07-10 10:27:20 -0700

    Completed Second Excercise

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -11,4 +11,15 @@ def reconstruct_trip(tickets, length):
     """
     # Your code here
 
+    route = [None] * length
+    loc = {}
+    for ticket in tickets:
+        loc[ticket.source] = ticket.destination
+    next = loc["NONE"]
+
+    for i in range(0, length):
+        route[i] = next
+        next = loc[next]
+
+
     return route
